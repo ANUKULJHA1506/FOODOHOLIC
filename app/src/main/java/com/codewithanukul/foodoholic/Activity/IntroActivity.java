b1934548a841860c293a2f050c73a5618158ce43
ANUKULJHA1506 <studious.anukul@gmail.com>
2022-05-29 19:44:06 +0530

    FOODOHOLIC APP

---
+++ app/src/main/java/com/codewithanukul/foodoholic/Activity/IntroActivity.java
@@ -0,0 +1,23 @@
+package com.codewithanukul.foodoholic.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.codewithanukul.foodoholic.R;
+
+public class IntroActivity extends AppCompatActivity {
+    ConstraintLayout startBtn ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        startBtn = findViewById(R.id.startBtn);
+
+        startBtn.setOnClickListener(view -> startActivity(new Intent(IntroActivity.this,MainActivity.class)));
+    }
+}
---
+++ app/src/main/java/com/codewithanukul/foodoholic/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.codewithanukul.foodoholic.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codewithanukul.foodoholic.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
